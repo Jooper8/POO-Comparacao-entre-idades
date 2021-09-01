5d94fd3dfcf2065f1bc95f258c6341121ee7e961
Jooper8 <joopereight@gmail.com>
2021-09-01 20:10:30 -0300

    Add project files.

---
+++ Dados de Pessoas/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados_de_Pessoas
+{
+    class Person
+    {
+        //Declaração das variáveis nome e idade da classe Person.
+        public string name;
+        public int age;
+    }
+}
---
+++ Dados de Pessoas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Dados_de_Pessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração das variáveis A e B da classe Person.
+            Person A, B;
+
+            //Declaração das pessoas A e B.
+            A = new Person();
+            B = new Person();
+
+            //Inserção do usuário sobre a pessoa A.
+            Console.WriteLine("Insira o nome da pessoa A:");
+            A.name = Console.ReadLine();
+            Console.WriteLine("Agora insira a idade da pessoa A:");
+            A.age = int.Parse(Console.ReadLine());
+
+            //Inserção do usuário sobre a pessoa B.
+            Console.WriteLine("Insira o nome da pessoa B:");
+            B.name = Console.ReadLine();
+            Console.WriteLine("Agora insira a idade da pessoa B:");
+            B.age = int.Parse(Console.ReadLine());
+
+            //Cálculo para identificar a pessoa mais velha.
+            if (A.age > B.age)
+            {
+                Console.WriteLine("{0} é mais velho(a) que {1}.", A.name, B.name);
+            }
+            else if (A.age < B.age)
+            {
+                Console.WriteLine("{0} é mais velho(a) que {1}.", B.name, A.name);
+            }
+            else
+            {
+                Console.WriteLine("Ambas as pessoas possuem a mesma idade.");
+            }
+        }
+    }
+}
